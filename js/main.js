046742c073772994922ff817bc158f40e9834feb
valehd <valehd.92@gmail.com>
2026-05-22 21:42:38 -0400

    modificacion en html, agrego estilos y dos funcionalidades

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//funcionalidad 1: alerta con input
+
+//obtener el texto del imput
+let texto= document.querySelector("#textoingresado")
+
+//obtener el boton
+let boton= document.querySelector("#crearTarea");
+
+//agregar el evento on click --> uso eventlistener
+boton.addEventListener("click", function(){
+    alert("agregando:"+ texto.value)
+})
+
+
+//funcionalidad 2: modificar p en tareas, dejarlo subrayado al hacer click
+
+//capturo el primer p de la clase tarea
+let tareaClick= document.querySelectorAll(".tarea p")
+
+tareaClick.forEach(function(tarea){
+    tarea.addEventListener("click",function(){
+         tarea.style.textDecoration = "line-through";
+    })
+})
+
+
+//funcionalidad 3: cursor sobre icono, cambia a otro icono
+
+
