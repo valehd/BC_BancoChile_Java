93555595c5342a4cd80a51c7d452cbcfdb03e62f
valehd <valehd.92@gmail.com>
2026-06-13 15:38:24 -0400

    subo core_personas_cuentas_bancarias

---
+++ Aplicacion.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+import clases.CuentaBancaria;
+import clases.Persona;
+
+public class Aplicacion{
+public static void main(String[] args) {
+    
+    Persona persona1 = new Persona("Vale", 25);
+    Persona persona2 = new Persona("Camila", 33);
+    Persona persona3 = new Persona("Junaito", 20);
+
+    CuentaBancaria cuenta1 = new CuentaBancaria(10000, persona1);
+    CuentaBancaria cuenta2 = new CuentaBancaria(25000, persona2);
+    CuentaBancaria cuenta3 = new CuentaBancaria(5000, persona3);
+
+
+      System.out.println("==========================================");
+    cuenta1.despliegaInformacion();
+    
+     System.out.println("==========================================");
+    //DEPOSITO
+    cuenta2.depositar(10000);
+    cuenta2.despliegaInformacion();
+    
+     System.out.println("==========================================");
+    //RETIRO
+    cuenta3.retirar(800);
+    cuenta3.despliegaInformacion();
+  
+    System.out.println("==========================================");
+
+
+ 
+}
+
+
+
+
+
+
+}
